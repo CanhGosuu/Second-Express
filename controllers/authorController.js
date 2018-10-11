9cc9eded017e25ca763ff26a4610cd3fca3dec4b
CanhGosuu <safetycanh@gmail.com>
2018-10-11 22:36:58 +0700

    vứt lifespan

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -112,12 +112,73 @@ module.exports = {
     }
   ],
   //display delete form on GET
-  author_delete_get: (req, res) => {
-    res.send("NOT INPLEMENTED: Author delete GET");
+  author_delete_get: (req, res, next) => {
+    // res.send("NOT INPLEMENTED: Author delete GET");
+    async.parallel(
+      {
+        author: function(callback) {
+          Author.findById(req.params.id).exec(callback);
+        },
+        authors_books: function(callback) {
+          Book.find({ author: req.params.id }).exec(callback);
+        }
+      },
+      function(err, results) {
+        if (err) {
+          return next(err);
+        }
+        if (results.author == null) {
+          // No results.
+          res.redirect("/catalog/authors");
+        }
+        // Successful, so render.
+        res.render("catalog/deletes/author_delete", {
+          title: "Delete Author",
+          author: results.author,
+          author_books: results.authors_books
+        });
+      }
+    );
   },
   // Handle Author delete on POST
-  author_delete_post: (req, res) => {
-    res.send("NOT INPLEMENTED: Author delete POST");
+  author_delete_post: (req, res, next) => {
+    // res.send("NOT INPLEMENTED: Author delete POST");
+    async.parallel(
+      {
+        author: function(callback) {
+          Author.findById(req.body.authorid).exec(callback);
+        },
+        authors_books: function(callback) {
+          Book.find({ author: req.body.authorid }).exec(callback);
+        }
+      },
+      function(err, results) {
+        if (err) {
+          return next(err);
+        }
+        // Success
+        if (results.authors_books.length > 0) {
+          // Author has books. Render in same way as for GET route.
+          res.render("catalog/deletes/author_delete", {
+            title: "Delete Author",
+            author: results.author,
+            author_books: results.authors_books
+          });
+          return;
+        } else {
+          // Author has no books. Delete object and redirect to the list of authors.
+          Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(
+            err
+          ) {
+            if (err) {
+              return next(err);
+            }
+            // Success - go to author list
+            res.redirect("/catalog/authors");
+          });
+        }
+      }
+    );
   },
   //display update form on GET
   author_update_get: (req, res) => {
